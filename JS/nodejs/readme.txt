29a3f3ac775857cebb3bde48a0b679df3d0baf5a
Siddhesh Thakarkar <103029308+Siddhesh071103@users.noreply.github.com>
2024-02-04 12:41:46 +0530

    Add files via upload

---
+++ JS/nodejs/readme.txt
@@ -0,0 +1,6 @@
+1) npm i -g express@4.17.0
+2) list down packages: npm ls
+3) npm uninstall express
+
+fs - file system
+url - fetch code from url
---
+++ NModules/index.js
@@ -0,0 +1,14 @@
+require('./models/db.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('cors')());
+app.use('/api/students', require('./routes/students'));
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
